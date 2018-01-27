0a03816a0697440145a0bbefb2c9ef55a00802e7
zhengxian.lzx <lizhengxian1991@126.com>
2018-01-27 12:31:03 +0800

    optimis input method

--- app/src/main/java/com/woyou/zxbrowser/MainActivity.java
+++ app/src/main/java/com/woyou/zxbrowser/MainActivity.java
@@ -1,15 +1,17 @@
 package com.woyou.zxbrowser;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 
 import com.woyou.zxbrowser.browser.IWebEventListener;
-import com.woyou.zxbrowser.common.Const;
 import com.woyou.zxbrowser.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements IWebEventListener {
@@ -23,7 +25,14 @@ public class MainActivity extends AppCompatActivity implements IWebEventListener
             WebView.setWebContentsDebuggingEnabled(BuildConfig.DEBUG);
         }
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mBinding.btnLoad.setOnClickListener((view) -> mBinding.webview.loadUrl(mBinding.addressBar.getText().toString()));
+        mBinding.addressBar.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                mBinding.webview.loadUrl(mBinding.addressBar.getText().toString());
+                totleShowSoftInput();
+                return true;
+            }
+            return false;
+        });
         mBinding.webview.setWebEventListener(this);
     }
 
@@ -54,5 +63,12 @@ public class MainActivity extends AppCompatActivity implements IWebEventListener
     @Override
     public void onProgressChanged(WebView view, int newProgress) {
         mBinding.progressBar.setProgress(newProgress);
+    }
+    public void totleShowSoftInput(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                    InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }
     }
 }
