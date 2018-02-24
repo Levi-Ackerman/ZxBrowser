988d970b2c62a3203d119421ff072078ac6b1f66
zhengxian.lzx@alibaba-inc.com <zhengxian.lzx@alibaba-inc.com>
2018-02-24 14:55:36 +0800

    use search

--- zxutil/src/main/java/com/woyou/baseconfig/ConstConfig.java
+++ zxutil/src/main/java/com/woyou/baseconfig/ConstConfig.java
@@ -5,7 +5,7 @@ package com.woyou.baseconfig;
  */
 
 public class ConstConfig {
-    public static final String HOME_PAGE_URL = "file:///android_asset/home.html";
+    public static final String HOME_PAGE_URL = "file:///android_asset/search.html";
     public static final String URL_REG = "(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]";
     public static final String BAIDU_SEARCH_PREFIX = "https://www.baidu.com/s?wd=";
 }
