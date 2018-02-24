3d866c50a68c6510406c385bcffeb67522718367
zhengxian.lzx@alibaba-inc.com <zhengxian.lzx@alibaba-inc.com>
2018-02-24 20:39:41 +0800

    fix keywork be host

--- zxutil/src/main/java/com/woyou/baseconfig/ConstConfig.java
+++ zxutil/src/main/java/com/woyou/baseconfig/ConstConfig.java
@@ -6,6 +6,7 @@ package com.woyou.baseconfig;
 
 public class ConstConfig {
     public static final String HOME_PAGE_URL = "file:///android_asset/search.html";
-    public static final String URL_REG = "((https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]";
+    public static final String URL_REG = "(((^https?:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)$";
+//    public static final String URL_REG = "((https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]";
     public static final String BAIDU_SEARCH_PREFIX = "https://www.baidu.com/s?wd=";
 }
